e1899c97dffc4c2bab10e6b424961e9ee64d7d59
dumas45 <alexei.skorodumov@gmail.com>
2020-04-04 15:03:04 +0300

    Correct Greatest common divisor

--- src/main/java/study/algs4/GreatestCommonDivisor.java
+++ src/main/java/study/algs4/GreatestCommonDivisor.java
@@ -1,7 +1,5 @@
 package study.algs4;
 
-import java.util.concurrent.ThreadLocalRandom;
-
 public class GreatestCommonDivisor {
     /**
      * Greatest common divisor
@@ -31,32 +29,5 @@ public class GreatestCommonDivisor {
         }
 
         return max;
-    }
-
-    private static void test() {
-        ThreadLocalRandom tlr = ThreadLocalRandom.current();
-        for (int i = 0; i < 1000000; i++) {
-            int a = tlr.nextInt(1, 1111);
-            int b = tlr.nextInt(1, 1111);
-            if (gcd(a, b) != gcd_greedy(a, b)) {
-                throw new RuntimeException(a + ", " + b);
-            }
-        }
-        System.out.println("OK");
-    }
-
-    private static int gcd_greedy(int a, int b) {
-        if (a < 1 || b < 1) throw new IllegalArgumentException();
-
-        for (int c = Math.max(a, b); c > 0 ; c--) {
-            if (a % c == 0 && b % c == 0) {
-                return c;
-            }
-        }
-        return 1;
-    }
-
-    public static void main(String[] args) {
-        test();
     }
 }
